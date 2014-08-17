fbb56502298a1b21487114056500752f485576c6
Tom Diggle <tom@tomdiggle.com>
2014-08-17 18:47:27 +0100

    Change location of class in a href link
    Signed-off-by: Tom Diggle <tom@tomdiggle.com>

--- source/_plugins/generate_categories.rb
+++ source/_plugins/generate_categories.rb
@@ -210,7 +210,7 @@ module Jekyll
         category_dir = GenerateCategories.category_dir(base_dir, category)
         # Make sure the category directory begins with a slash.
         category_dir = "/#{category_dir}" unless category_dir =~ /^\//
-        "<a class='category' href='#{category_dir}/'>#{category}</a>"
+        "<a href='#{category_dir}/' class='category'>#{category}</a>"
       end
 
       case categories.length
