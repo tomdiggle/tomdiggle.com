8c94982104649dfa2e45419478529787a9a36f3d
Tom Diggle <tom@tomdiggle.com>
2014-10-20 10:40:14 +0100

    Change contact form now uses FormData
    Signed-off-by: Tom Diggle <tom@tomdiggle.com>

--- source/assets/contact.js
+++ source/assets/contact.js
@@ -1,6 +1,7 @@
 document.getElementById('form_contact').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    var formData = new FormData(document.getElementById('form_contact'));
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/assets/contact.php');
     xhr.onreadystatechange = function() {
@@ -14,19 +15,8 @@ document.getElementById('form_contact').addEventListener('submit', function(even
                 document.getElementById('form_contact_message').innerHTML = '<p class="error">' + response.msg + '</p>';
             }
         } else {
-            document.getElementById('form_contact_message').innerHTML = '<p class="error">There was an error when trying to send your message. Please try again, or send an email directly at <a href="tom+website@tomdiggle.com">tom+website@tomdiggle.com</a>.</p>';
+            document.getElementById('form_contact_message').innerHTML = '<p class="error">There was an error when trying to send your message. Please try again, or send an email directly at <a href="mailto:tom+website@tomdiggle.com">tom+website@tomdiggle.com</a>.</p>';
         }
     }
-
-    var data = '';
-    var elements = document.getElementById('form_contact').elements;
-    for (var i = 0, len = elements.length; i < len; ++i) {
-        if (elements[i].tagName === 'INPUT' || elements[i].tagName === 'TEXTAREA' || elements[i].tagName === 'SELECT') {
-            data += elements[i].name + '=' + encodeURIComponent(elements[i].value) + '&';
-        }
-    }
-    data = data.substring(0, data.length - 1);
-
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send(data);
+    xhr.send(formData);
 });
